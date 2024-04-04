9ae5be7f947c07eb041a293652d620baa285d1ed
Kevin Whitley <krwhitley@gmail.com>
2024-04-04 17:56:01 -0500

    testing root

--- _worker.js
+++ _worker.js
@@ -1,5 +1,9 @@
 import { AutoRouter } from 'itty-router'
 
+const apiRouter = AutoRouter({ base: '/api' })
+  .get('/', () => 'Hello World.')
+  .get('/params/:a/:b?', ({ a, b }) => ({ a, b }))
+
 export default AutoRouter()
-.all('/api/*', () => `Hello World`)
+.all('/api/*', () => [1,2,3])
 .all('*', (req, env) => env.ASSETS.fetch(req))
