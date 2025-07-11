38263861ab6b5e763f1ab3a63f9f55bcfd6b2c2a
Kevin Whitley <krwhitley@gmail.com>
2025-07-11 08:20:12 -0500

    add itty scripts to console with examples

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,32 @@
 // https://vitepress.dev/guide/custom-theme
 import { chroma } from 'itty-chroma'
+import { fetcher } from 'itty-fetcher'
+import { connect } from 'itty-sockets'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import './style.scss'
+
+const scriptLoaded = (name: string, icon: string = '✅') => {
+  const parts = name.includes('itty')
+    ? [chroma.bold.color('#f0b'), name, chroma.clear]
+    : [name]
+
+  chroma.log(
+    icon,
+    ...parts.flat(),
+    chroma.bg('#555').bold.color('#aaa').padding('0.2em 0.6em 0.2em').radius('0.4rem').size('7px'),
+    'available in console'
+  )
+}
+
+const example = (description: string) =>
+  chroma.log(
+    chroma.font('serif').size('1.3em')('try this:\n'),
+    chroma.darkGrey.size('0.8em'),
+    description,
+  )
+
 
 export default {
   extends: DefaultTheme,
@@ -17,6 +40,27 @@ export default {
     if (typeof window !== 'undefined') {
       // @ts-ignore
       window.chroma = chroma
+      // @ts-ignore
+      window.fetcher = fetcher
+      // @ts-ignore
+      window.connect = connect
+
+      scriptLoaded('itty-chroma')
+      example(`chroma.bold.color('#f0b').log('Hello!')`)
+
+      scriptLoaded('itty-fetcher')
+      example(`fetcher().get('https://ittysockets.io/stats').then(console.log)`)
+
+      scriptLoaded('itty-sockets')
+      example(`connect('itty.dev/playground', { echo: true })
+        .on('message', e => chroma.magenta.log('received:', chroma.clear, e.message))
+        .on('join', e => chroma.dodgerBlue.log('someone joined the channel:', e))
+        .on('leave', e => chroma.salmon.log('someone left the channel:', e))
+        .send('Hello World!')
+        .send([1, 2, 3])
+        .send({ foo: 'bar' })
+        `
+      )
     }
   }
 } satisfies Theme
